598978ef51a697e4a1c8a30dbc025f4250c8315e
Benjamin McLemore <51517288+Cheimonophile@users.noreply.github.com>
2024-08-25 16:54:40 -0400

    add tailwind

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-
 import { createRoot } from 'react-dom/client';
 
 
@@ -8,14 +7,10 @@ import { createRoot } from 'react-dom/client';
 export const App: FC<{}> = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1 className="bg-blue-500">App</h1>
     </div>
   );
 }
-
-
-
-
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
