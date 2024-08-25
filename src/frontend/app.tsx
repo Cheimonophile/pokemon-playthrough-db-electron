b7addcfe07efb25bd5124ad51f081682196834e5
Benjamin McLemore <51517288+Cheimonophile@users.noreply.github.com>
2024-08-25 19:19:30 -0400

    basic shadcn

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -6,8 +6,10 @@ import { createRoot } from 'react-dom/client';
 
 export const App: FC<{}> = () => {
   return (
-    <div>
-      <h1 className="bg-blue-500">App</h1>
+    <div className="flex flex-col">
+
+      {/** Navigation Menu */}
+      <h1 className="bg-primary text-primary-foreground">App</h1>
     </div>
   );
 }
