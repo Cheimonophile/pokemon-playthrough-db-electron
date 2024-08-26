096ae4f840c08f31f11f2475cd14251cba4a959f
Benjamin McLemore <51517288+Cheimonophile@users.noreply.github.com>
2024-08-25 20:26:49 -0400

    Navigation Menu

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,15 +1,25 @@
 import { FC } from "react";
 import { createRoot } from 'react-dom/client';
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "./components/shadcn/ui/navigation-menu";
 
 
 
 
 export const App: FC<{}> = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col p-2 gap-2">
 
       {/** Navigation Menu */}
-      <h1 className="bg-primary text-primary-foreground">App</h1>
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+
     </div>
   );
 }
